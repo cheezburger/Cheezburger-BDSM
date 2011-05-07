8c1b69be9e12a332ffba613219461d91ce747a84
James Thigpen <james.r.thigpen@gmail.com>
2011-05-07 13:18:57 -0700

    Build scripts now generate nuget packages.

--- build/build.ps1
+++ build/build.ps1
@@ -7,5 +7,5 @@ include .\master_build.ps1
 include .\test_build.ps1
 #include .\stage_application.ps1
 #include .\build_msi_installer.ps1
-#include .\package_deployment.ps1
-task default -depends compile, test #, stage #, test, generate_installer, package
+include .\package_deployment.ps1
+task default -depends compile, test, nuget_pack #, stage #, test, generate_installer, package
--- build/package_deployment.ps1
+++ build/package_deployment.ps1
@@ -1,9 +1,17 @@
 properties {
 	$package_dir = "$build_dir\package\"
+	$nuget = "$tool_dir\nuget\NuGet.exe"
+	$nuget_deploy_dir = "$base_dir\nuget"
 }
 
 task package {
 	if (Test-Path $package_dir) { Remove-Item -Recurse -Force $package_dir }
     New-Item $package_dir -ItemType Directory | Out-Null
 	Copy-Item "$stage_dir\*.msi" $package_dir
+}
+
+task nuget_pack {
+	New-Item "$nuget_deploy_dir\lib" -ItemType Directory | Out-Null
+	Copy-Item "$build_output_dir\Cheezburger.SchemaManager.dll" "$nuget_deploy_dir\lib\"
+	& $nuget pack "$base_dir\nuget\Cheezburger.SchemaManager.nuspec"
 }
