1f12c1b970957e0e676fb953befac0d4049a5891
Bobby Johnson <bobby.johnson@gmail.com>
2011-05-07 11:13:11 -0700

    initial commit of simple build script

---
+++ build/build.ps1
@@ -0,0 +1,11 @@
+properties {
+$build_number = if($env:BUILD_NUMBER) {$env:BUILD_NUMBER.Split('.')[2] } else { "0" }
+$version = if($env:BUILD_NUMBER) {$env:BUILD_NUMBER} else { "1.0.0.1" }
+}
+
+include .\master_build.ps1
+#include .\test_build.ps1
+#include .\stage_application.ps1
+#include .\build_msi_installer.ps1
+#include .\package_deployment.ps1
+task default -depends compile #, stage #, test, generate_installer, package
